19d1546c5c84933412d1df835146641bed5925b3
Konstantinos Tsiotas <t.k@gmail.com>
2016-05-29 17:18:12 +0300

    Moving enemies started

---
+++ Assets/Scripts/PathFollower.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFollower: MonoBehaviour {
+	public Transform[] path;
+	public float speed = 5.0f;
+	public float reachDist = 1.0f;
+	public int currentPoint = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float dist = Vector3.Distance (path [currentPoint].position, transform.position);
+
+
+		transform.LookAt( Vector3.MoveTowards (transform.position, path [currentPoint].position, Time.deltaTime * speed) );
+
+
+		transform.position = Vector3.MoveTowards (transform.position, path [currentPoint].position, Time.deltaTime * speed);
+
+		Debug.Log( Vector3.MoveTowards (transform.position, path [currentPoint].position, Time.deltaTime * speed));
+			
+		if (dist <= reachDist) {
+			currentPoint++;
+		}
+
+		if (currentPoint >= path.Length) {
+			currentPoint = 0;
+		}
+	}
+
+	void onDrawGizmos(){
+		if (path.Length > 0) {
+			for (int i = 0; i < path.Length; i++) {
+				if (path [i] != null) {
+					Gizmos.DrawSphere (path [i].position, reachDist);
+				}
+			}
+		}
+	}
+
+}
